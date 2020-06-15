6e1aa96306dd2d7ef3591280ab3cdd0c98144836
176496 <176496@INHYDDIS5HBFL33.AMERICAS.GLOBAL.NTTDATA.COM>
2020-06-15 11:11:45 +0530

    Static add commit

---
+++ addition/src/addition/add.java
@@ -0,0 +1,14 @@
+package addition;
+
+public class add {
+	public static void main(String args[])
+	{
+		int a,b,c;
+		a=2;
+		b=3;
+		c=a+b;
+		System.out.println("Addition of two numbers " +c);
+		
+	}
+
+}
